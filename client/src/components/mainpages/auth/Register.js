698ebc7965dc6dcc826b739919cabacbe70304c6
Rohan <>
2021-12-11 20:36:54 +0530

    added frontend

---
+++ client/src/components/mainpages/auth/Register.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+
+function Register() {
+    const [user, setUser] = useState({
+        name:'', email:'', password: ''
+    })
+
+    const onChangeInput = e =>{
+        const {name, value} = e.target;
+        setUser({...user, [name]:value})
+    }
+
+    const registerSubmit = async e =>{
+        e.preventDefault()
+        try {
+            await axios.post('/user/register', {...user})
+
+            localStorage.setItem('firstLogin', true)
+
+            
+            window.location.href = "/";
+        } catch (err) {
+            alert(err.response.data.msg)
+        }
+    }
+
+    return (
+        <div className="login-page">
+            <form onSubmit={registerSubmit}>
+                <h2>Register</h2>
+                <input type="text" name="name" required
+                placeholder="Name" value={user.name} onChange={onChangeInput} />
+
+                <input type="email" name="email" required
+                placeholder="Email" value={user.email} onChange={onChangeInput} />
+
+                <input type="number" name="role" required
+                placeholder="Enter 1 for Owner Or 0 for Customer" value={user.role} onChange={onChangeInput} />
+
+                {/* <select  style={{width:"100%",height:"40px"}} value={user.role} onChange={onChangeInput}>
+                    
+                    <option value="1">Owner</option>
+                    <option value="0">Customer</option>
+                </select> */}
+
+                
+                <input type="password" name="password" required autoComplete="on"
+                placeholder="Password" value={user.password} onChange={onChangeInput} />
+
+                <div className="row">
+                    <button type="submit">Register</button>
+                    <Link to="/login">Login</Link>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Register
