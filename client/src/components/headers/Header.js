5d6d919fa5b04aef011098b7f5d67d449f397a42
Rohan <>
2022-04-21 20:00:58 +0530

    added file upload

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -1,10 +1,13 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState,useEffect} from 'react'
 import {GlobalState} from '../../GlobalState'
 import Menu from './icon/menu.svg'
 import Close from './icon/close.svg'
 import Cart from './icon/cart.svg'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
+import Upload from './icon/upload.svg'
+import { createWorker } from "tesseract.js";
+
 
 function Header() {
     const state = useContext(GlobalState)
@@ -46,17 +49,54 @@ function Header() {
     // const [sort, setSort] = state.productsAPI.sort
     
     const [search, setSearch] = state.productsAPI.search
-
-
     // const handleCategory = e => {
     //     setCategory(e.target.value)
     //     setSearch('')
     // }
-
-
     const styleMenu = {
         left: menu ? 0 : "-100%"
     }
+/////////////////////////ocr//////////////////////////////////
+  const [ocr, setOcr] = useState("");
+  const [imageData, setImageData] = useState(null);
+  const worker = createWorker({
+    logger: (m) => {
+      console.log(m);
+    },
+  });
+
+  setInterval(function () {console.clear()}, 50000);
+  const convertImageToText = async () => {
+    if (!imageData) return;
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const {
+      data: { text },
+    } = await worker.recognize(imageData);
+    setOcr(text);
+    setSearch(text.toLowerCase())
+  };
+
+  useEffect(() => {
+    convertImageToText();
+  }, [imageData]);
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    if(!file)return;
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const imageDataUri = reader.result;
+      console.log({ imageDataUri });
+      setImageData(imageDataUri);
+    };
+    reader.readAsDataURL(file);
+  }
+  
+  
+  if(ocr) console.log(ocr)
+
 
     return (
         <header style={{backgroundColor:"rgb(255,225,65)",display:"flex",justifyContent:"space-around"}}>
@@ -71,9 +111,18 @@ function Header() {
                 </h1>
             </div>
 
-            <div>
+            <div style={{display:'flex',alignContent:'center',alignItems:'center' }}>
             <input type="text" value={search} style={{textAlign:"center",width:"35vw",resize: 'vertical'}} placeholder="Search your Favourite products here!"
             onChange={e => setSearch(e.target.value.toLowerCase())} />
+            <div class="upload-btn-wrapper " style={{justifyContent:'center',alignItems:'center',cursor: 'pointer'}}>
+                        <button class="btn" style={{backgroundColor:'yellow'}}><img src={Upload} alt="" width="20" /></button>
+                        <form action="">
+                        <input type="file" name="myfile" style={{cursor:'pointer'}}
+                        //  onChange={(e)=>{console.log(e.target.files[0]); setimg(e.target.files[0])}} 
+                        onChange={handleImageChange}/>
+                        {/* <input type="submit" value="upload"/> */}
+                        </form>
+                    </div>
             </div>
 
             <ul style={styleMenu}>
