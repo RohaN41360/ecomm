75b22d2126aca568e24ee53f8b6c637a3158284d
Rohan <>
2022-04-26 18:36:52 +0530

    added 404 page

--- client/src/components/mainpages/utils/not_found/NotFound.js
+++ client/src/components/mainpages/utils/not_found/NotFound.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import './not_found.css'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useHistory
+  } from "react-router-dom";
 
 function NotFound() {
+    const history = useHistory();
     return (
         <div>
-            404 | Not Found
+            <div id='oopss'>
+    <div id='error-text'>
+        <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404" />
+        <span>404 PAGE</span>
+        <p class="p-a">
+           . The page you were looking for could not be found</p>
+        <p class="p-b">
+            
+        </p>
+        <Link to="/" style={{textDecoration:'none',color:"ButtonText"}}>Go To Home Page</Link>
+
+
+       {/* / <button class="button button1" style={{backgrondColor:ye''}}>{history.goBack}Go Back</button> */}
+    </div>
+</div>
+  
+  
+
         </div>
     )
 }
