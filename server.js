4033b4f8a02f65507e22c37684dad18dcf01779f
Rohan Raghunath Pawar <72547854+RohaN41360@users.noreply.github.com>
2023-06-03 17:35:08 +0530

    Update server.js
    app.listen outside the production

--- server.js
+++ server.js
@@ -45,3 +45,8 @@ app.listen(PORT, () =>{
     console.log('Server is running on port', PORT)
 })
 }
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () =>{
+    console.log('Server is running on port', PORT)
+})
